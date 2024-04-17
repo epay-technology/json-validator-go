9d4f5e0c57819aab1e9645737599d53652c1873c
Niki Zakariassen <niki@epay.technology>
2024-04-17 10:52:22 +0200

    Added tests for validation of arrays of structs

---
+++ Tests/ArrayOfStructs_test.go
@@ -0,0 +1,95 @@
+package Tests
+
+import (
+	"errors"
+	"github.com/epay-technology/json-validator-go/JsonValidator"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func Test_it_can_validate_valid_arrays_of_structs(t *testing.T) {
+	// Arrange
+	var errorBag *JsonValidator.ErrorBag
+	jsonString := []byte(`{"Child": [{"Data": "ab"}, {"Data": "bc"}]}`)
+	type testDataChild struct {
+		Data string `validation:"len:2"`
+	}
+
+	type testDataParent struct {
+		Child []testDataChild `validation:"required"`
+	}
+
+	// Act
+	_, err := JsonValidator.Validate[testDataParent](jsonString)
+	_ = errors.As(err, &errorBag)
+
+	// Assert
+	require.NoError(t, err)
+	require.Equal(t, 0, errorBag.CountErrors())
+}
+
+func Test_it_can_validate_invalid_arrays_of_structs(t *testing.T) {
+	// Arrange
+	var errorBag *JsonValidator.ErrorBag
+	jsonString := []byte(`{"Child": [{"Data": "ab"}, {"Data": "bcd"}]}`)
+	type testDataChild struct {
+		Data string `validation:"len:2"`
+	}
+
+	type testDataParent struct {
+		Child []testDataChild `validation:"required"`
+	}
+
+	// Act
+	_, err := JsonValidator.Validate[testDataParent](jsonString)
+	_ = errors.As(err, &errorBag)
+
+	// Assert
+	require.Error(t, err)
+	require.True(t, errorBag.HasFailedKeyAndRule("Child.1.Data", "len"))
+	require.Equal(t, 1, errorBag.CountErrors())
+}
+
+func Test_it_can_validate_valid_arrays_of_arrays_of_structs(t *testing.T) {
+	// Arrange
+	var errorBag *JsonValidator.ErrorBag
+	jsonString := []byte(`{"Child": [[{"Data": "aa"}, {"Data": "bb"}], [{"Data": "cc"}]]}`)
+	type testDataChild struct {
+		Data string `validation:"len:2"`
+	}
+
+	type testDataParent struct {
+		Child [][]testDataChild `validation:"required"`
+	}
+
+	// Act
+	_, err := JsonValidator.Validate[testDataParent](jsonString)
+	_ = errors.As(err, &errorBag)
+
+	// Assert
+	require.NoError(t, err)
+	require.Equal(t, 0, errorBag.CountErrors())
+}
+
+func Test_it_can_validate_invalid_arrays_of_arrays_of_structs(t *testing.T) {
+	// Arrange
+	var errorBag *JsonValidator.ErrorBag
+	jsonString := []byte(`{"Child": [[{"Data": "ab"}, {"Data": "bcd"}], [{"Data": "abc"}, {"Data": "bc"}]]}`)
+	type testDataChild struct {
+		Data string `validation:"len:2"`
+	}
+
+	type testDataParent struct {
+		Child [][]testDataChild `validation:"required"`
+	}
+
+	// Act
+	_, err := JsonValidator.Validate[testDataParent](jsonString)
+	_ = errors.As(err, &errorBag)
+
+	// Assert
+	require.Error(t, err)
+	require.True(t, errorBag.HasFailedKeyAndRule("Child.0.1.Data", "len"))
+	require.True(t, errorBag.HasFailedKeyAndRule("Child.1.0.Data", "len"))
+	require.Equal(t, 2, errorBag.CountErrors())
+}
