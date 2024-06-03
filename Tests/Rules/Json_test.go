19c2b43c3a2f577fca26aba933cb8db3090ae895
Nikolaj Boel Jensen <nbj@lindekrogen.dk>
2024-06-03 09:10:59 +0200

    Add json validation rule

---
+++ Tests/Rules/Json_test.go
@@ -0,0 +1,52 @@
+package Rules
+
+import (
+	"errors"
+	"fmt"
+	"github.com/epay-technology/json-validator-go/JsonValidator"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func Test_it_can_validate_using_json_rule(t *testing.T) {
+	// Setup
+	cases := []struct {
+		jsonString []byte
+		shouldFail bool
+	}{
+		{[]byte(`{"Data": "{\"valid\":\"json\"}"}`), false},
+		{[]byte(`{"Data": ""}`), true},
+		{[]byte(`{"Data": [1,2,3]}`), true},
+		{[]byte(`{"Data": {}}`), true},
+		{[]byte(`{"Data": "hello world"}`), true},
+		{[]byte(`{"Data": 123}`), true},
+		{[]byte(`{"Data": null}`), true},
+		{[]byte(`{"Data": true}`), true},
+	}
+
+	type testData struct {
+		Data any `validation:"json"`
+	}
+
+	for i, testCase := range cases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			// Arrange
+			var errorBag *JsonValidator.ErrorBag
+
+			// Act
+			var data testData
+			err := JsonValidator.New().Validate(testCase.jsonString, &data)
+			_ = errors.As(err, &errorBag)
+
+			// Assert
+			if testCase.shouldFail {
+				require.True(t, errorBag != nil)
+				require.True(t, errorBag.HasFailedKeyAndRule("Data", "json"))
+				require.Equal(t, 1, errorBag.CountErrors())
+			} else {
+				require.True(t, errorBag == nil)
+				require.Equal(t, 0, errorBag.CountErrors())
+			}
+		})
+	}
+}
