bfc7a4f78316a3ef68d39107dfdc9b663fe0d974
Niki Zakariassen <niki@epay.technology>
2024-11-27 11:04:54 +0100

    Updated the url rule to reject ip and localhost and non http and https domains

--- Tests/Rules/Url_test.go
+++ Tests/Rules/Url_test.go
@@ -16,6 +16,9 @@ func Test_it_can_validate_using_url_rule(t *testing.T) {
 	}{
 		{[]byte(`{"Data": "https://www.google.dk/"}`), false},
 		{[]byte(`{"Data": "http://www.google.dk"}`), false},
+		{[]byte(`{"Data": "http://google.dk"}`), false},
+		{[]byte(`{"Data": "https://google.dk"}`), false},
+		{[]byte(`{"Data": "http://www.google.dk:8000"}`), true},
 		{[]byte(`{"Data": "www.google.dk"}`), true},
 		{[]byte(`{"Data": "google.dk"}`), true},
 		{[]byte(`{"Data": [1,2,3]}`), true},
@@ -24,6 +27,19 @@ func Test_it_can_validate_using_url_rule(t *testing.T) {
 		{[]byte(`{"Data": 123}`), true},
 		{[]byte(`{"Data": null}`), true},
 		{[]byte(`{"Data": true}`), true},
+		{[]byte(`{"Data": "ftp://ftp.example.com"}`), true},
+		{[]byte(`{"Data": "tcp://example.com:54321"}`), true},
+		{[]byte(`{"Data": "udp://example.com:6000"}`), true},
+		{[]byte(`{"Data": "wss://example.com"}`), true},
+		{[]byte(`{"Data": "https://127.0.0.1"}`), true},
+		{[]byte(`{"Data": "http://127.0.0.1"}`), true},
+		{[]byte(`{"Data": "127.0.0.1"}`), true},
+		{[]byte(`{"Data": "localhost"}`), true},
+		{[]byte(`{"Data": "http://localhost"}`), true},
+		{[]byte(`{"Data": "http://localhost"}`), true},
+		{[]byte(`{"Data": "https://localhost"}`), true},
+		{[]byte(`{"Data": "http://localhost:8000"}`), true},
+		{[]byte(`{"Data": "https://localhost:8000"}`), true},
 	}
 
 	type testData struct {
